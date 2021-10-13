9b3e3531bdb1a725bca7443f4be9b65d5738d5c7
Joaquin Casabal <joaquincasabal@hotmail.com>
2021-10-13 08:53:43 -0300

    Add files via upload

---
+++ Clase 4 Joaquin Casabal.py
@@ -0,0 +1,6 @@
+print("Los dias de la semana son: ")
+for indice in ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]:
+    print(indice)
+print("Los meses del año son:")
+for indice in ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]:
+    print(indice)
